2c22253da7218284ae9da70fa829dca2436070fc
LINC365 <43487712+LINC365@users.noreply.github.com>
2021-04-09 17:50:12 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -15,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         int a=0;
         //提交由android studio
         //提交由xmxmxsfuser2
+        //相同1
+        //webUser1
+        //webUser2
+        //相同2
+        int b=0;
+        //相同3
     }
 }
