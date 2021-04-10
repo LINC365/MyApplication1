a2c199be6992296a0a81856ff6e5c55857498d8e
刘坤林1 <1655710110@qq.com>
2021-04-10 09:49:51 +0800

    提交了新的Aclass

---
+++ app/src/main/java/com/example/myapplication1/A.java
@@ -0,0 +1,15 @@
+package com.example.myapplication1;
+
+/**
+ * 作品人:create By shaoDong on 2021/4/10 09: 39
+ * 邮箱：myu17635728243@163.com
+ * note: 哪里没有朴素、善良和真理，哪里也就谈不上有伟大.
+ * desc: this class is create By 分支1
+ *
+ * @author 分支1创建的class A
+ **/
+public class A {
+    public static String result ( ) {
+        return "这是分支1的功能，测试成功";
+    }
+}
