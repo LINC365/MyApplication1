049944231ee31abd896f573b90cc198dcbb4fafb
刘坤林1 <1655710110@qq.com>
2021-04-10 10:06:39 +0800

    master状态下新建的class

---
+++ app/src/main/java/com/example/myapplication1/Master.java
@@ -0,0 +1,12 @@
+package com.example.myapplication1;
+
+/**
+ * 作品人:create By shaoDong on 2021/4/10 10: 05
+ * 邮箱：myu17635728243@163.com
+ * note: 哪里没有朴素、善良和真理，哪里也就谈不上有伟大.
+ * desc:
+ *
+ * @author
+ **/
+class Master {
+}
